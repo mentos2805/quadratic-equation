10a8e5e709028bb7468ae0a5f5686863a07b8a1f
mentos2805 <mentos28059@gmail.com>
2018-09-26 00:39:39 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var str = equation.replace(/ /g, "");
+  var arr = ( str.match(/-?\d+/g) );
+  var a = Number(arr[0]);
+  var b = Number(arr[2]);
+  var c = Number(arr[3]);
+  var d = b*b - 4*a*c;
+  var result=[];
+
+  if(d>0){
+      result.push( Math.round( ( -b + Math.sqrt(d) ) / (2*a) ) ); 
+      result.push( Math.round( ( -b-Math.sqrt(d) ) / (2*a) ) ); 
+  }else if (d==0){
+      result.push( Math.round( -b / (2*a) ) );
+  }
+
+  return result.sort(function( a, b ){return a - b});
 }
